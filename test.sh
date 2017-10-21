7a6f35f54ad6cf9fce7961189abe0ef014cf8a2f
Ori Hoch <ori@uumpa.com>
2017-10-22 00:00:05 +0300

    sleep to let socks proxy start

--- test.sh
+++ test.sh
@@ -4,4 +4,4 @@ source ./test_functions.sh
 
 info "Running ssh-socks-proxy tests"
 
-provision_test_environment && ssh_socks_proxy_test
+provision_test_environment && sleep 2 && ssh_socks_proxy_test
